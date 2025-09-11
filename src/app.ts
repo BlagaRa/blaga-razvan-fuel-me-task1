4f760aa2b2dc4f9e7cbf2456ddc3a0c00554cc7c
blagarazvan <razvanblaga10@gmail.com>
2025-09-12 00:06:14 +0300

    update the error handeling throwing the error until catched in main and printed

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,6 @@ const fetchData=async<T>(url:string):Promise<T>=>{
         if(!res.ok){
             throw new Error("HTTP error");
         }
-
         const data:T=await res.json();
 
         return data;
@@ -40,20 +39,25 @@ let posts:Post[]=[];
 
 const fetchAllData=async()=>{
     try {
-        
         users=await fetchData<User[]>("https://jsonplaceholder.typicode.com/users");
         posts=await fetchData<Post[]>("https://jsonplaceholder.typicode.com/posts");
+       
 
     } catch (error) {
         console.log("Error in fetchAllData");
-        console.error("Failed to fetch the request",error)
+        throw error;
     }
 }
 
 const main=async()=>{
-    await fetchAllData();
-    console.log(users);
-    console.log(posts);
+    try {
+        await fetchAllData();
+        console.log(users);
+        console.log(posts);
+    } catch (error) {
+        console.log(error);
+        
+    }
 }
 
 main()
