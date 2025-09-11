3b0346258414c57367aaeff779d8092d56c5509a
blagarazvan <razvanblaga10@gmail.com>
2025-09-12 00:19:52 +0300

    changed the name of the second method to processingData like in the problem. encapsulated everithing besides the interfaces in ReportGenerator and changed function and proprietis to private and public based on where we want to use them

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ interface User{
     email:string
 }
 
- interface Post{
+interface Post{
     id:number,
     userId:number,
     title:string,
@@ -18,53 +18,50 @@ interface UserWithPosts{
     posts:Post[]
 }
 
-const fetchData=async<T>(url:string):Promise<T>=>{
-    try {
-        const res=await fetch(url);
+class ReportGenerator{
+    private users:User[]=[];
+    private posts:Post[]=[];
+    
 
-        if(!res.ok){
-            throw new Error("HTTP error");
+    private fetchData=async<T>(url:string):Promise<T>=>{
+        try {
+            const res=await fetch(url);
+
+            if(!res.ok){
+                throw new Error("HTTP error");
+            }
+
+            const data:T=await res.json();
+            return data;
+        } catch (error) {
+            console.log("error in fetchData");
+            throw error;
         }
-        const data:T=await res.json();
+    }
 
-        return data;
-    } catch (error) {
-        console.log("error in fetchData");
-        throw error;
+    
+
+    private processingData=async()=>{
+        try {
+            this.users=await this.fetchData<User[]>("https://jsonplaceholder.typicode.com/users");
+            this.posts=await this.fetchData<Post[]>("https://jsonplaceholder.typicode.com/posts");
+        } catch (error) {
+            console.log("Error in processingData");
+            throw error;
+        }
+    }
+
+    public main=async():Promise<void>=>{
+        try {
+            await this.processingData();
+            console.log(this.users);
+            console.log(this.posts);
+        } catch (error) {
+            console.log(error);
+            
+        }
     }
 }
 
-let users:User[]=[];
-let posts:Post[]=[];
-
-const fetchAllData=async()=>{
-    try {
-        users=await fetchData<User[]>("https://jsonplaceholder.typicode.com/users");
-        posts=await fetchData<Post[]>("https://jsonplaceholder.typicode.com/posts");
-       
-
-    } catch (error) {
-        console.log("Error in fetchAllData");
-        throw error;
-    }
-}
-
-const main=async()=>{
-    try {
-        await fetchAllData();
-        console.log(users);
-        console.log(posts);
-    } catch (error) {
-        console.log(error);
-        
-    }
-}
-
-main()
-
-
-
-
-
-
-
+const main=new ReportGenerator();
+main.main();
