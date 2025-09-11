00ca740c1e0bb99c5d0e3a6a7aa6d3b23192ccf7
blagarazvan <razvanblaga10@gmail.com>
2025-09-11 16:03:49 +0300

    created fetchData function made it async so that i can make the request to fetch the data

--- src/app.ts
+++ src/app.ts
@@ -16,3 +16,24 @@ interface UserWithPosts{
     user:User,
     posts:Post[]
 }
+const fetchData=async<T>(url:string):Promise<T>=>{
+    try {
+        const res=await fetch(url);
+
+        if(!res.ok){
+            throw new Error("HTTP error");
+        }
+
+        const data:T=await res.json();
+
+        return data;
+
+    } catch (error) {
+        console.log("error in fetchData");
+        throw error;
+    }
+}
+
+
+
+
