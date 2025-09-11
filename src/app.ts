ad28e6c056f0a3a62d1d100c8b19031496dcb6b7
blagarazvan <razvanblaga10@gmail.com>
2025-09-11 15:29:33 +0300

    Adding the interfaces for User Post and UserWithPosts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log("Antonio")
+interface User{
+    id:number,
+    name:string,
+    username:string,
+    email:string
+}
+
+ interface Post{
+    id:number,
+    title:string,
+    body:string
+
+}
+
+interface UserWithPosts{
+    user:User,
+    posts:Post[]
+}
