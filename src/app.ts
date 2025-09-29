684670a22d6d70a7a30d1c724a198003101d9fe3
BlagaRa <razvanblaga10@gmail.com>
2025-09-29 19:19:58 +0300

    Changed the functionality adding fetch comments and making the whole application faster and easier tho scale becuase right now you have to go throw each array once reducing the loop inside another loop reducing the iterations from 500*100*10 to 500+100+10

--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,39 @@
+//Interface For Users
 interface User{
     id:number,
     name:string,
     username:string,
     email:string,
-    address:{
-        street:string,
-        suite:string,
-        city:string,
-        zipcode:string,
-        geo:{
-            lat:string,
-            lng:string
-        }
-    }
-    phone:string,
-    website:string,
-    company:{
-        name:string,
-        catchPhrase:string,
-        bs:string,
-    }
+    
 }
 
+//Interface For Posts
 interface Post{
     userId:number,
     id:number,
     title:string,
-    body:string
+    body:string,
+    comments:Comment[]
 }
 
+//Interface For UsersWithPosts
 interface UserWithPosts{
     userId:number,
     name:string,
     username:string,
     email:string,
-    posts:{
-            postId:number,
-            title:string,
-            body:string
-    }[];  
+    posts:Post[] 
 }
 
+//Interface for Comments
+interface Comment{
+    postId:number,
+    id:number,
+    name:string,
+    body:string
+}
+
+//The clouser function for loggers
 function createLogger(context:string){
     function loggingFunction(message:string){
         console.log(`[${context}] ${message}`)
@@ -48,10 +41,31 @@ function createLogger(context:string){
     return loggingFunction;
 }
 
+//Interface for making the array for Posts and to put the index for each users
+interface UserPosts{
+    posts:Post[]
+}
+
+
+//Interface for making the array Comments and to asign to the index of Posts
+interface PostComments{
+    comments:Comment[];
+}
+
+//Interface for asigning Posts With comments to the array with the index userId
+interface UserPostsWithComments{
+    postsWithComments:Post[];
+}
+
 class ReportGenerator{
     private users:User[]=[];
     private posts:Post[]=[];
     private usersWithPosts:UserWithPosts[]=[];
+
+    private comments:Comment[]=[];
+
+    //Counter to count the iterations and to make sure i am not making the same mistakes with loop in loop
+    private count=0;
     
     private loggerFetching=createLogger("DataFetching");
     private loggerProcessing=createLogger("Processing");
@@ -72,63 +86,187 @@ class ReportGenerator{
         }
     }
 
-    
-
-    private processingData=async():Promise<void>=>{
+    //I have separated the fetching functions just to be easier to extend without modifications in other functions
+    private fetchUsers=async():Promise<User[]>=>{
         try {
-            this.users=await this.fetchData<User[]>("https://jsonplaceholder.typicode.com/users");
-            this.posts=await this.fetchData<Post[]>("https://jsonplaceholder.typicode.com/posts");
+            return await this.fetchData<User[]>("https://jsonplaceholder.typicode.com/users");  
         } catch (error) {
-            console.log("Error in processingData");
-            console.log(error);
+            throw error;
+            
         }
     }
 
-    private combineUsersWithPosts=():void=>{
+    private fetchPosts=async():Promise<Post[]>=>{
         try {
-            this.usersWithPosts=this.users.map(user=>{
-                const existingPosts:Post[]=this.posts.filter(post=>post.userId===user.id);
+            return await this.fetchData<Post[]>("https://jsonplaceholder.typicode.com/posts");  
+        } catch (error) {
+            throw error;
+        }
+    }
 
-                const posts:{postId:number,title:string,body:string}[]=existingPosts.map(post=>{
-                    return {postId:post.id,
-                            title:post.title,
-                            body:post.body
-                    }
-                })
-                
-                return {userId:user.id,
-                        email:user.email,
-                        username:user.username,
-                        name:user.name,
-                        posts
+    private fetchComments=async():Promise<Comment[]>=>{
+        try {
+            return this.fetchData<Comment[]>("https://jsonplaceholder.typicode.com/comments");
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    //Fetch All to respect the question with promiseAll
+    private fetchAll=async()=>{
+        try {
+            this.count++;
+
+            const [users,posts,comments]=await Promise.all([
+                this.fetchData<User[]>("https://jsonplaceholder.typicode.com/users"),
+                this.fetchData<Post[]>("https://jsonplaceholder.typicode.com/posts"),
+                this.fetchData<Comment[]>("https://jsonplaceholder.typicode.com/comments"),
+            ])
+
+            this.users=users;
+            this.posts=posts;
+            this.comments=comments;
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    //This is the function that i will
+    //  call in combineUsersWithPosts to be 
+    // able to asign the posts in 10 iterations 
+    // throw the users and not needing to make 100
+    //  iterrations for each users
+    //I am using ! because typescript thinks that everithing can be undefined
+    private postsForUsers=(posts:Post[]):UserPosts[]=>{
+        const userPosts:UserPosts[]=[];
+        for(let i=0;i<posts.length;i++){
+            this.count++;
+            if(typeof userPosts[posts[i]!.userId]==='undefined'){
+                userPosts[posts[i]!.userId]={posts:[]};
+            }
+            userPosts[posts[i]!.userId]!.posts.push(posts[i]!);
+        }
+        return userPosts;
+    }
+
+    //Same this as the upper function but for Posts With comments
+    private commentsForPosts=(comments:Comment[]):PostComments[]=>{
+        const postComments:PostComments[]=[];
+
+        for(let i=0;i<comments.length;i++){
+            this.count++;
+
+            if(typeof postComments[comments[i]!.postId]==='undefined'){
+                postComments[comments[i]!.postId]={comments:[]}
+            }
+
+            postComments[comments[i]!.postId]!.comments.push(comments[i]!);
+        }
+
+        return postComments;
+        
+    }
+
+    //Here i am combining the users with the posts
+    private combineUsersWithPosts=(users:User[]):UserWithPosts[]=>{
+        try {
+            const posts=this.postsForUsers(this.posts);
+
+            return users.map(user=>{
+                this.count++;
+
+                return {
+                    userId:user.id,
+                    username:user.username,
+                    email:user.email,
+                    name:user.name,
+                    posts:posts[user.id]!.posts||[]
                 }
             })
-
         } catch (error) {
             console.log("Error in combineUsersWithPosts");
             throw error;
         }
     }
 
-    
-    
+    //Here i am combining posts with comments
+    private combinePostsWithComments=(posts:Post[]):Post[]=>{
+        const comments=this.commentsForPosts(this.comments);
 
-    private postingFunction=(userWithPosts:UserWithPosts):void=>{
-        console.log(userWithPosts);
+        return posts.map(post=>{
+            this.count++;
+            return{
+                userId:post.userId,
+                id:post.id,
+                title:post.title,
+                body:post.body,
+                comments:comments[post.id]!.comments || []
+            }
+        })
     }
 
+    //This is the same function as commetsForPosts and postsForUsers
+    //I had to put the userId inside Posts With comments to have a foreign key to be able to reach UsersWithPosts 
+    private forUsersWithPostsWithComments=(postsWithComments:Post[]):UserPostsWithComments[]=>{
+        const userPostsWithComments:UserPostsWithComments[]=[];
 
+        for(let i=0;i<postsWithComments.length;i++){
+            this.count++;
+            const postWithComments=postsWithComments[i]!;
 
-    public main=async():Promise<void>=>{
+            if(typeof userPostsWithComments[postWithComments.userId]==='undefined'){
+                userPostsWithComments[postWithComments.userId]={postsWithComments:[]}
+            }
+
+            userPostsWithComments[postWithComments.userId]?.postsWithComments.push(postWithComments);
+        }
+
+        return userPostsWithComments;
+    }
+
+    //Here i am combining the usersWithPosts with the PostsWithComments
+    //This is an old version because now i am doing the modifications directly on the posts and dont need this function
+    //I have reduced the iterations with 700 like that
+    private combineUsersWithPostsWithComments=(usersWithPosts:UserWithPosts[])=>{
+        const postss=this.forUsersWithPostsWithComments(this.posts);
+
+        return usersWithPosts.map(userWithPosts=>{
+            this.count++;
+            return{
+                userId:userWithPosts.userId,
+                name:userWithPosts.name,
+                username:userWithPosts.username,
+                email:userWithPosts.email,
+                posts:postss[userWithPosts.userId]!.postsWithComments||[],
+            }
+        })
+    }
+
+    public generateUserArray=async():Promise<void>=>{
         try {
-            this.loggerFetching("Fetching users and posts...");
-            await this.processingData();
+            this.loggerFetching("Fetching users and posts and comments...");
+            // this.users=await this.fetchUsers();
+            // this.posts=await this.fetchPosts();
+            // this.comments=await this.fetchComments();
+            await this.fetchAll();
             
             this.loggerProcessing("Combining every users with their posts...");
-            this.combineUsersWithPosts();
+            const postWithComments=this.combinePostsWithComments(this.posts);
+            this.posts=postWithComments;  
 
-            this.loggerProcessing("Printing users with their posts");
-            this.usersWithPosts.forEach(user=>this.postingFunction(user))
+            this.usersWithPosts=this.combineUsersWithPosts(this.users);
+
+            this.loggerProcessing("Combine every posts with their comments...")
+
+            this.usersWithPosts.forEach(finalUserWithPosts=>{
+                console.log(finalUserWithPosts.userId,finalUserWithPosts.email,finalUserWithPosts.name,finalUserWithPosts.username);
+                finalUserWithPosts.posts.forEach(post=>{
+                    console.log(post);
+                })
+            })
+            
+            console.log(`The number of iterations is:${this.count}`);
 
             console.log("Success");
         } catch (error) {
@@ -138,4 +276,4 @@ class ReportGenerator{
 }
 
 const main=new ReportGenerator();
-main.main();
+main.generateUserArray(); 
